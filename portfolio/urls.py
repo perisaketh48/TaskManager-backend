3d89e58fa4d92205a4316aa10d20371c9e398c7b
saketh.peri@nexiilabs.com <perisaketh545@gmail.com>
2025-08-07 15:05:22 +0530

    changes added for deployment fix

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('data-get/',views.sendmail, name='send-email' )
+   path('data-get/',views.send_whatsapp_message, name='send_whatsapp_message' )
 
 ]
