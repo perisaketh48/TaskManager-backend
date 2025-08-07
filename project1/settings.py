f9c08799c205534885a4d2bd5a28aee00cfe359d
saketh.peri@nexiilabs.com <perisaketh545@gmail.com>
2025-08-07 14:08:20 +0530

    changes added for cors error

--- project1/settings.py
+++ project1/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1', 'https://saketh-peri-13.vercel.app').split(',')
 
 # Application definition
 INSTALLED_APPS = [
